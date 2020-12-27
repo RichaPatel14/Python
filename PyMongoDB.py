40dec554388db55b31ffce62272fe2c05a2237fb
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-27 19:57:18 +0530

    Update PyMongoDB.py
    All delete/insert/find/update in one file

--- PyMongoDB.py
+++ PyMongoDB.py
@@ -6,4 +6,48 @@ mydb = myclient['test']
 
 mycol = mydb['customers']
 
-mycol.drop()
+
+def insert(val):
+    x = mycol.insert_one(val)
+    print(x.inserted_id)
+
+
+def find(val):
+    for x in mycol.find(val):
+        print(x)
+
+
+def delete(val):
+    x = mycol.delete_one(val)
+    print(x.deleted_count)
+
+
+def update(val, nval):
+    x = mycol.update_one(val, nval)
+    if x:
+        print("Successfully Update")
+
+
+y = input("What you want insert/delete/find/Update: ")
+if y.upper() == "INSERT":
+    x = 0
+    for x in mycol.find({}, {"_id": 1}):
+        pass
+    x = x['_id'] + 1
+
+    val = {
+        "_id": x, "name": input("Enter your name: "), "Country": input("Enter Your Country: ")
+    }
+    insert(val)
+elif y.upper() == "DELETE":
+    val = {"name": input("Enter your name: ")}
+    delete(val)
+elif y.upper() == "FIND":
+    val = {"name": input("Enter your name: ")}
+    find(val)
+elif y.upper() == "UPDATE":
+    val = {"name": input("Enter your name: ")}
+    nval = {"$set": {"Country": input("Enter your Country: ")}}
+    update(val, nval)
+else:
+    print("Please enter Insert/Delete/Find: ")
