2720e7ce3d0016d811a111de52bc2e71b2e766ac
RichaPatel14 <50714706+RichaPatel14@users.noreply.github.com>
2020-12-27 17:15:48 +0530

    Update PyMongoDB.py
    Insert row

--- PyMongoDB.py
+++ PyMongoDB.py
@@ -5,3 +5,12 @@ myclient = pymo.MongoClient("mongodb://localhost:27017")
 mydb = myclient['test']
 
 mycol = mydb['customers']
+
+dict = {
+    "name": "Richa",
+    "Country: "Canada",
+}
+x = mycol.insert_one(dict)
+
+if x:
+    print("Enter Successfully")
